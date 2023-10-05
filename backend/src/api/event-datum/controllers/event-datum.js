d1311e4c23884c419e0f4a286bc38bfa0fe59018
Adhitya Nantish <adhitya@Adhitya.bbrouter>
2023-10-05 18:58:22 +0530

    Reinitialize Git repository

---
+++ backend/src/api/event-datum/controllers/event-datum.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-datum controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::event-datum.event-datum');
---
+++ backend/src/api/event-datum/routes/event-datum.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-datum router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::event-datum.event-datum');
