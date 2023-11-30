5af66ecb48ab7d458c5a083c01be7b96bb0e641f
adhitya nantish <adhitya2k3@gmail.com>
2023-11-30 18:16:38 +0530

    added some validation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     
 
-<footer className="bg-white ">
+<footer className="bg-white mt-64">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
